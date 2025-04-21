f4061208b6e88ec48e7779e98aae10835c803c20
BorisVaneev <47080432+BorisVaneev@users.noreply.github.com>
2025-04-21 12:51:21 +0300

    Update run_migrations.py

--- run_migrations.py
+++ run_migrations.py
@@ -1,12 +1,13 @@
 from app import db
 from flask_migrate import Migrate, upgrade
+from app import app  # Импортируем объект Flask-приложения
 
 # Инициализация миграции
-migrate = Migrate()
+migrate = Migrate(app, db)  # Передаем app и db в Migrate
 
 def run_migrations():
     try:
-        upgrade()
+        upgrade()  # Запуск миграций
         print("✅ Migration successful!")
     except Exception as e:
         print(f"❌ Migration failed: {e}")
